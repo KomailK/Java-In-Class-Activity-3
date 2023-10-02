f3f84399ab5fcac5ac3eff1aadb9ba6edf75f2f3
KomailK <98917001+KomailK@users.noreply.github.com>
2023-10-02 15:55:43 -0400

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -8,9 +8,9 @@ public class MyArrayList {
         Scanner myObj = new Scanner(System.in);  
         System.out.println("Insert numeric values with spaces between");
         String values = myObj.nextLine(); 
-        System.out.println("Average number: "+Convert(values));}
+        System.out.println("Average number: "+Average(values));}
 
-    public static int Convert(String value1) {
+    public static int Average(String value1) {
         int res=0;
         String myArray[]= value1.split(" ");
         for(String element: myArray)
